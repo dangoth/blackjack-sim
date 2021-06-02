5682540a953c39523f41f9aa0473db0cca46088e
dangoth <38084164+dangoth@users.noreply.github.com>
2021-06-02 18:54:54 +0200

    Update blackjack.py
    v 0.3 - separated scoring method, simplified/remove certain methods, added multiprocessing with 100k iterations per CPU core

--- blackjack.py
+++ blackjack.py
@@ -9,7 +9,7 @@ class Player:
         self.score = {"win":0, "lose":0, "draw":0}
         self.hand = []
         self.target = target
-        self.playing = False
+        self.isPlaying = False
 
     def __str__(self):
         return f'{self.score["win"]} wins, {self.score["lose"]} losses and {self.score["draw"]} draws'
@@ -24,7 +24,6 @@ class Player:
         deck.numCards -= 1
         #print(f"Player's new hand is {player.hand}")
 
-
     def decide(self, player):
         currSum = player.getScore(player)
         #print(f"Hand sum in player decide function is {currSum}")
@@ -34,8 +33,8 @@ class Player:
                 #print(f"Changed ace into 1, new hand is {player.hand}")
                 player.decide(player)
             else:
-                player.playing = False
-                print("Player busted")
+                player.isPlaying = False
+                #print("Player busted")
                 return
         elif currSum < self.target:
             #print("Player decides to hit")
@@ -47,15 +46,6 @@ class Player:
 
 class Dealer(Player):
 
-    def __init__(self, target):
-        super().__init__(target)
-
-    def getScore(self, dealer):
-        return sum(dealer.hand)
-
-    def drawCard(self, deck, player):
-        super().drawCard(deck, player)
-
     def decide(self, dealer):
         currSum = dealer.getScore(dealer)
         #print(f"Hand sum in dealer decide function is {currSum}")
@@ -64,8 +54,8 @@ class Dealer(Player):
                 dealer.hand[dealer.hand.index(11)] = 1
                 dealer.decide(dealer)
             else:
-                dealer.playing = False
-                print("Dealer busted")
+                dealer.isPlaying = False
+                #print("Dealer busted")
                 return
         #soft 17 - dealer hits if sum is 17 with ace in hand
         elif currSum == self.target:
@@ -95,11 +85,9 @@ class Deck:
         self.numCards = 0
 
     def shuffle(self):
-        self.cards = []
-        for i in range(self.numDecks):
-            self.cards += [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] * 4
-        self.numCards = self.numDecks * 52
-
+        self.cards = ([2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] * 4) * self.numDecks
+        self.numCards = 52 * self.numDecks
+        return self.cards, self.numCards
 
 class Game:
 
@@ -108,67 +96,57 @@ class Game:
         self.playerCount = 0
         self.playerTarget = 0
 
-    def play_game(self, players, deck):
-        random.seed()
-        print(f"Current num of cards is {deck.numCards}, left side is {deck.numCards / (deck.numDecks*52)}, right side is {float(deck.percentToShuffle)}, eval is {(deck.numCards / (deck.numDecks*52)) < float(deck.percentToShuffle)}")
-        print(deck.percentToShuffle)
-        if deck.numCards / (deck.numDecks*52) < float(deck.percentToShuffle):
-            print("Shuffling deck")
-            deck.shuffle()
-            #print(f"Shuffled deck is {deck.cards}")
-        for player in self.players:
-            player.playing = True
-        for _ in range(2):
-            for player in players:
-                player.drawCard(deck, player)
-        for player in self.players[1:]:
-            #print(f"New player choses, his hand is {player.hand}")
-            player.decide(player)
-
-        #dealer
-        #print("Dealer chooses")
-        players[0].decide(players[0])
-
-        print("Post game evaluation")
+    def score(self, game):
         scores = []
-        for player in self.players:
-            if player.playing:
+        for player in game.players:
+            if player.isPlaying:
                 scores.append(player.getScore(player))
             else:
                 scores.append(0)
-        for player in players:
+        for player in game.players:
             player.hand = []
         winning = max(scores)
-        print(f"Scores are {scores}, winning is {winning}")
-        #dealer has the highest score
-        if winning == scores[0]:
-            if scores.count(scores[0]) == 1:
-                print("Dealer wins")
-                players[0].score['win'] += 1
-                for i in range(1, len(players)):
-                    players[i].score['lose'] += 1
-            else:
-                #dealer drew with 1 or more players
-                print("Draw between dealer and 1 or more players")
-                for i in range(1, len(players)):
-                    if scores[i] == scores[0]:
-                        players[i].score['draw'] += 1
-                    else:
-                        players[i].score['lose'] += 1
-        #dealer lost
-        elif winning > scores[0]:
-            print("One or more players won")
-            for i in range(len(scores)):
+        if scores.count(winning) > 1:
+            for i in range(len(game.players)):
                 if scores[i] == winning:
-                    players[i].score['win'] += 1
+                    game.players[i].score['draw'] += 1
                 else:
-                    players[i].score['lose'] += 1
-
+                    game.players[i].score['lose'] += 1
+        elif scores.count(winning) == 1:
+            for i in range(len(game.players)):
+                if scores[i] == winning:
+                    game.players[i].score['win'] += 1
+                else:
+                    game.players[i].score['lose'] += 1
+        """print scores block
         for _ in range(len(players)):
             if _ == 0:
                 print(f"Dealer has {players[_]}")
             else:
-                print(f"Player {_} has {players[_]}")
+                print(f"Player {_} has {players[_]}")"""
+
+
+    def play_game(self, players, deck):
+        for i in range(100000):
+            print(f"Game {i}")
+            random.seed()
+            #print(f"Current num of cards is {deck.numCards}, left side is {deck.numCards / (deck.numDecks*52)}, right side is {float(deck.percentToShuffle)/100}, eval is {(deck.numCards / (deck.numDecks*52)) < float(deck.percentToShuffle)/100}")
+            if deck.numCards / (deck.numDecks*52) < float(deck.percentToShuffle)/100:
+                deck.cards, deck.numCards = deck.shuffle()
+                #print(f"Shuffled deck is {deck.cards}")
+            for player in self.players:
+                player.isPlaying = True
+            for _ in range(2):
+                for player in players:
+                    player.drawCard(deck, player)
+            for player in self.players[1:]:
+                #print(f"New player choses, his hand is {player.hand}")
+                player.decide(player)
+            #print("Dealer chooses")
+            players[0].decide(players[0])
+
+            #print("Post game evaluation")
+            game.score(game)
 
 
 if __name__ == "__main__":
@@ -205,5 +183,17 @@ if __name__ == "__main__":
     deck.percentToShuffle = get_int("percentage of cards left to shuffle the deck", 95)
     deck.shuffle()
 
-    for i in range(100000):
-        game.play_game(game.players, deck)
+    processes = []
+
+    for i in range(os.cpu_count()):
+        print(f"Process {i+1} registered")
+        processes.append(Process(target=game.play_game(game.players, deck)))
+
+    for process in processes:
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    for player in game.players:
+        print(player)
